52454317de51da0a9358735ea4074ecd6b5a6bab
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-02-20 13:36:33 -0500

    Changed Rocky 9 tests to include CIS hardening and order of tests.

--- roles/authentication_proxy/molecule-tests.sh
+++ roles/authentication_proxy/molecule-tests.sh
@@ -2,20 +2,23 @@
 
 set -e
 
-MOLECULE_BOX="rocky8_cis" molecule test
-MOLECULE_BOX="rocky8_cis" MOLECULE_DUO_UPGRADE=true molecule test
+MOLECULE_BOX="rocky9_cis" molecule test
+MOLECULE_BOX="rocky9_cis" MOLECULE_DUO_UPGRADE=true molecule test
+
 MOLECULE_BOX="debian12_base" molecule test
 MOLECULE_BOX="debian12_base" MOLECULE_DUO_UPGRADE=true molecule test
+
 MOLECULE_BOX="ubuntu2404_base" molecule test
 MOLECULE_BOX="ubuntu2404_base" MOLECULE_DUO_UPGRADE=true molecule test
 
-MOLECULE_BOX="rocky9_base" molecule test
-MOLECULE_BOX="rocky9_base" MOLECULE_DUO_UPGRADE=true molecule test
+MOLECULE_BOX="rocky8_cis" molecule test
+MOLECULE_BOX="rocky8_cis" MOLECULE_DUO_UPGRADE=true molecule test
 
 MOLECULE_BOX="debian11_base" molecule test
 MOLECULE_BOX="debian11_base" MOLECULE_DUO_UPGRADE=true molecule test
 
 MOLECULE_BOX="ubuntu2204_base" molecule test
 MOLECULE_BOX="ubuntu2204_base" MOLECULE_DUO_UPGRADE=true molecule test
+
 MOLECULE_BOX="ubuntu2004_base" molecule test
 MOLECULE_BOX="ubuntu2004_base" MOLECULE_DUO_UPGRADE=true molecule test
